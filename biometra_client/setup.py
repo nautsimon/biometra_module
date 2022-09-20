367ff1f2ce88e5c2a7d811bf05fe18f0a30b8d9d
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2022-09-19 22:11:07 -0500

    initial placeholders

---
+++ biometra_client/setup.py
@@ -0,0 +1,48 @@
+import os
+from setuptools import setup, find_packages
+
+
+install_requires = []
+with open('requirements.txt') as reqs:
+    for line in reqs.readlines():
+        req = line.strip()
+        if not req or req.startswith('#'):
+            continue
+        install_requires.append(req)
+
+package_name = 'biometra_client'
+
+setup(
+    name='biometra_client',
+    version='0.0.1',
+    packages=find_packages(),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=install_requires,
+    zip_safe=True,
+    python_requires=">=3.8",
+    maintainer='',
+    maintainer_email='ravescovi@anl.gov',
+    description='',
+    url='https://github.com/AD-SDL/biometra_module.git', 
+    license='MIT License',
+    entry_points={ 
+        'console_scripts': [
+             'biometra_client = biometra_client.biometra_client:main_null',
+             'tcp_driver = biometra_client.tcp_driver:main_null',
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
