92750bcc0795e875ba4ab8d4d1ebb596c3757f28
Dozgulbas <dozgulbas@anl.gov>
2022-11-21 16:35:20 -0600

    Clean up

--- biometra_description/setup.py
+++ biometra_description/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 from glob import glob
 
-package_name = 'pf400_description'
+package_name = 'biometra_description'
 
 setup(
     name=package_name,
@@ -20,14 +20,14 @@ setup(
 
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Doga Ozgulbas, Rafael Vescovi',
-    maintainer_email='dozgulbas@anl.gov, ravescovi@anl.gov',
+    maintainer='Doga Ozgulbas',
+    maintainer_email='dozgulbas@anl.gov',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'pf400_description_client = pf400_description.pf400_description_client:main'
+            'biometra_description_client = biometra_description.biometra_description_client:main'
         ],
     },
 )
