eae0ab7f43dad347ec3965a3f7fde1f4143a2435
Dozgulbas <dozgulbas@anl.gov>
2023-02-23 16:48:29 -0600

    adds ability to check status, temp, and lid status. fixes some driver issues

--- biometra_driver/biometra_driver/biometra_driver.py
+++ biometra_driver/biometra_driver/biometra_driver.py
@@ -16,6 +16,10 @@ class biometra_trobot():
 
         self.block_cmds = BiometraLibrary.DeviceExtComClasses.BlockClasses.BlockCmds(self.settings.CommunicationSettings, self.device_desc)
         self.block_n = BiometraLibrary.DeviceExtComClasses.BlockClasses.BlockDataClasses.BlockNumber(1)
+        
+        self.tcda_cmds = BiometraLibrary.DeviceExtComClasses.SystemClasses.TcdaClasses.TcdaCmds(self.settings.CommunicationSettings, self.device_desc)
+        
+        self.program_cmds = BiometraLibrary.DeviceExtComClasses.ProgClasses.ProgramCmds(self.settings.CommunicationSettings, self.device_desc)
 
         # self.fileInfo = BiometraLibrary.DeviceExtComClasses.DeviceComClasses.DeviceComParams
         self.DataSetList = BiometraLibrary.HelperClasses.DataSetHelperClasses.DataSetList
@@ -30,13 +34,13 @@ class biometra_trobot():
         return 0
 
     def find_device(self):
-        n, avail = self.info.GetAllComAvailableDevices()   
+        n, avail = self.info.GetAllComAvailableDevices()
         device_desc = avail.get_DataList().get_Item(0).DeviceInfos.DeviceDescriptionInfo
         return device_desc
     
     def login_user(self):
         login = BiometraLibrary.DeviceExtComClasses.LoginOutClasses.LoginOutCmds(self.settings.CommunicationSettings,self.device_desc)
-        user = BiometraLibrary.DeviceExtComClasses.LoginOutClasses.UserClasses.UserDataClasses.UserInitials('ADM')
+        user =  BiometraLibrary.DeviceExtComClasses.LoginOutClasses.UserClasses.UserDataClasses.UserInitials('ADM')
         passwd = BiometraLibrary.DeviceExtComClasses.LoginOutClasses.UserClasses.UserDataClasses.UserPassword('Admin')
         err = login.LoginUser(self.device_desc,user,passwd)
         return self.check_error(err)
@@ -55,27 +59,70 @@ class biometra_trobot():
     def get_state(self):
         pass
 
-    def list_programs():
-        err, program_list = program_cmds.GetProgramOverview(device_desc,user)
+    def list_programs(self):
+        err, program_list = self.program_cmds.GetProgramOverview(self.device_desc, self.user)
 
     def create_pcr_program():
         pass
 
-    def run_pcr_program(prog):
+    def run_pcr_program(self, prog):
         prog_type = BiometraLibrary.DeviceExtComClasses.ProgClasses.ProgDataClasses.ProgEditClasses.EnProgramType.TYPE_PROGRAM
         program_n = BiometraLibrary.DeviceExtComClasses.ProgClasses.ProgDataClasses.ProgEditClasses.ProgramNumber(prog, prog_type)
-        err = block_cmds.StartProgramOnBlock(device_desc, user, program_n, block_n, True)
-        check_error(err)
+        err = self.block_cmds.StartProgramOnBlock(self.device_desc, self.user, program_n, self.block_n, True)
+        self.check_error(err)
 
-    def stop_program():
-        err = block_cmds.StopProgramOnBlock(device_desc, block_n)
-        check_error(err)
+    def stop_program(self):
+        err = self.block_cmds.StopProgramOnBlock(self.device_desc, self.block_n)
+        self.check_error(err)
     
     def open_lid(self):
         self.block_cmds.OpenMotLid(self.device_desc,self.block_n)
 
     def close_lid(self):
         self.block_cmds.CloseMotLid(self.device_desc,self.block_n)
+        
+    def get_lid_state(self):
+        err, status = self.tcda_cmds.GetMotLidState(self.device_desc, self.block_n)
+        self.check_error(err)
+        if str(status) == "0000 0000 0000 0011":
+            print("The lid is open on thermocycler " + f"{self.device_desc}")
+        elif str(status) == "0000 0000 0000 0101":
+            print("The lid is closed on thermocycler " + f"{self.device_desc}")
+        else:
+            print("The lid is currently moving on thermocycler " + f"{self.device_desc}")
+
+
+    def check_temp_left(self):
+        err, temp = self.tcda_cmds.GetBlockTempLeft(self.device_desc, self.block_n)
+        self.check_error(err)
+        print("The left temprature of thermocycler " + f"{self.device_desc}" + " is " +f"{temp}")
+        return temp
+
+    def check_temp_right(self):
+        err, temp = self.tcda_cmds.GetBlockTempRight(self.device_desc, self.block_n)
+        self.check_error(err)
+        print("The right temprature of thermocycler " + f"{self.device_desc}" + " is " +f"{temp}")
+        return temp
+
+    def check_temp_middle(self):
+        err, temp = self.tcda_cmds.GetBlockTempMiddle(self.device_desc, self.block_n)
+        self.check_error(err)
+        print("The middle temprature of thermocycler " + f"{self.device_desc}" + " is " +f"{temp}")
+        return temp
+    
+    def check_temp_lid(self):
+        err, temp = self.tcda_cmds.GetHeatedLidTemp(self.device_desc, self.block_n)
+        self.check_error(err)
+        print("The lid temprature of thermocycler " + f"{self.device_desc}" + " is " +f"{temp}")
+        return temp
+
+    def check_temp_all(self):
+        left = self.check_temp_left()
+        right = self.check_temp_right()
+        middle = self.check_temp_middle()
+        all_temps = [left, middle, right]
+        print("The combined tempratures of thermocycler " + f"{self.device_desc}" + " is " +f"{all_temps[0]}, {all_temps[1]}, {all_temps[2]}")
+        return all_temps
 
 if __name__ == "__main__":
     test = biometra_trobot()
--- biometra_driver/biometra_driver/test.py
+++ biometra_driver/biometra_driver/test.py
@@ -1,4 +1,7 @@
 
+# for linux 
+#sudo apt install mono-devel 
+#pip install pythonnet
 
 import os
 import clr
@@ -30,28 +33,41 @@ block_n = BiometraLibrary.DeviceExtComClasses.BlockClasses.BlockDataClasses.Bloc
 
 program_cmds = BiometraLibrary.DeviceExtComClasses.ProgClasses.ProgramCmds(settings.CommunicationSettings, device_desc)
 pcrProgram = BiometraLibrary.DeviceExtComClasses.ProgClasses.ProgDataClasses.PcrProgram
+program_file_worker = BiometraLibrary.FileClasses.FileWorkClasses.DeviceFileWorkClasses.ProgramFileWorker
 
-def login_user():  
-    login_cmds = BiometraLibrary.DeviceExtComClasses.LoginOutClasses.LoginOutCmds(settings.CommunicationSettings,device_desc)
-    return check_error(login_cmds.LoginUser(device_desc,user,passwd))
-   
-login_user()
+login_cmds = BiometraLibrary.DeviceExtComClasses.LoginOutClasses.LoginOutCmds(settings.CommunicationSettings,device_desc)
+
+tcda_cmds = BiometraLibrary.DeviceExtComClasses.SystemClasses.TcdaClasses.TcdaCmds(settings.CommunicationSettings, device_desc)
+
+data_set_list = BiometraLibrary.HelperClasses.DataSetHelperClasses.DataSetList
+
 
 def check_error(err):
     if err:
         print(err)
 
+def login_user():  
+    return check_error(login_cmds.LoginUser(device_desc,user,passwd))
+   
+   
+   
+login_user()
+## to here is "connect", log, check
+
 def list_programs():
     err, program_list = program_cmds.GetProgramOverview(device_desc,user)
     print(program_list.ToString())
+
+def create_program():
+    checkStateResult = program_file_worker.ReadAllProgramTemplateInfosToShow(data_set_list)
 
 def create_pcr_program():
     pass
 
 def run_pcr_program(prog):
-prog_type = BiometraLibrary.DeviceExtComClasses.ProgClasses.ProgDataClasses.ProgEditClasses.EnProgramType.TYPE_PROGRAM
-program_n = BiometraLibrary.DeviceExtComClasses.ProgClasses.ProgDataClasses.ProgEditClasses.ProgramNumber(prog, prog_type)
-err = block_cmds.StartProgramOnBlock(device_desc, user, program_n, block_n, True)
+    prog_type = BiometraLibrary.DeviceExtComClasses.ProgClasses.ProgDataClasses.ProgEditClasses.EnProgramType.TYPE_PROGRAM
+    program_n = BiometraLibrary.DeviceExtComClasses.ProgClasses.ProgDataClasses.ProgEditClasses.ProgramNumber(prog, prog_type)
+    err = block_cmds.StartProgramOnBlock(device_desc, user, program_n, block_n, True)
     check_error(err)
 
 def stop_program():
@@ -69,4 +85,68 @@ def lid_close():
 def create_incubation():
     pass
 
+def get_params_data():
+    err, set_list = tcda_cmds.GetBlockParamsDataSet(device_desc)
+    check_error(err)
+    print("DATA SET LIST", set_list)
+    temp = str(set_list)
+    sub = temp.split(";")
+    print("LIST", sub)
 
+def get_status():
+    err, status = tcda_cmds.GetBlockState(device_desc, block_n)
+    check_error(err)
+    print('STATUS', status)
+
+def get_lid_state():
+    err, status = tcda_cmds.GetMotLidState(device_desc, block_n)
+    check_error(err)
+    if str(status) == "0000 0000 0000 0011":
+        print("The lid is open on thermocycler " + f"{device_desc}")
+    elif str(status) == "0000 0000 0000 0101":
+        print("The lid is closed on thermocycler " + f"{device_desc}")
+    else:
+        print("The lid is currently moving on thermocycler " + f"{device_desc}")
+
+#GetDeviceBlockInfo maybe just block info from gui?
+#GetHoldTime
+#GetMotLidState_Finished()
+#GetRemainingTime
+
+#block commands
+#RecieveBusyCmd
+#StartProgramOnBlock
+#UpdateDeviceNameFromDeviceDescription
+
+
+def check_temp_left():
+    err, temp = tcda_cmds.GetBlockTempLeft(device_desc, block_n)
+    check_error(err)
+    print("The left temprature of thermocycler " + f"{device_desc}" + " is " +f"{temp}")
+    return temp
+
+def check_temp_right():
+    err, temp = tcda_cmds.GetBlockTempRight(device_desc, block_n)
+    check_error(err)
+    print("The right temprature of thermocycler " + f"{device_desc}" + " is " +f"{temp}")
+    return temp
+
+def check_temp_middle():
+    err, temp = tcda_cmds.GetBlockTempMiddle(device_desc, block_n)
+    check_error(err)
+    print("The middle temprature of thermocycler " + f"{device_desc}" + " is " +f"{temp}")
+    return temp
+
+def check_temp_lid():
+    err, temp = tcda_cmds.GetHeatedLidTemp(device_desc, block_n)
+    check_error(err)
+    print("The lid temprature of thermocycler " + f"{device_desc}" + " is " +f"{temp}")
+    return temp
+
+def check_temp_all():
+    left = check_temp_left()
+    right = check_temp_right()
+    middle = check_temp_middle()
+    all_temps = [left, middle, right]
+    print("The combined tempratures of thermocycler " + f"{device_desc}" + " is " +f"{all_temps[0]}, {all_temps[1]}, {all_temps[2]}")
+    return all_temps
