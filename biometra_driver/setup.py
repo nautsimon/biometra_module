dedcdea3d5af7a1ab8b95a106d9775ae44d8beb3
abestroka <abestroka@gmail.com>
2023-03-22 16:58:07 -0500

    updates setup.py

--- biometra_driver/setup.py
+++ biometra_driver/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
-
+import os
+from glob import glob
 
 package_name = "biometra_driver"
 
@@ -7,7 +8,12 @@ setup(
     name="biometra_driver",
     version="0.0.1",
     packages=find_packages(),
-    package_data={package_name: ["dotnet/*.dll", "dotnet/*.xml"]},
+    data_files=[{package_name: ["dotnet/*.dll", "dotnet/*.xml"]},
+            ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+            ('share/' + package_name, ['package.xml']),
+            (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        ],
     install_requires=["pythonnet"],
     zip_safe=True,
     python_requires=">=3.8",
@@ -16,5 +22,9 @@ setup(
     description="",
     url="https://github.com/AD-SDL/biometra_module/biometra_driver",
     license="MIT License",
-    entry_points={},
+    entry_points={ 
+        'console_scripts': [
+            'biometra_driver = biometra_driver.biometra_driver.biometra:run_program',
+        ]
+    },
 )
