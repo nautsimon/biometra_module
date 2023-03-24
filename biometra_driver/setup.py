f280fb613e151d98ba38f77307e9e5589758780f
abestroka <abestroka@gmail.com>
2023-03-24 13:08:28 -0500

    resolves things addressed in comments, still need to test client

--- biometra_driver/setup.py
+++ biometra_driver/setup.py
@@ -8,10 +8,7 @@ setup(
     name="biometra_driver",
     version="0.0.1",
     packages=find_packages(),
-    data_files=[('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-            ('share/' + package_name, ['package.xml']),
-        ],
+    package_data={package_name: ["dotnet/*.dll", "dotnet/*.xml"]},
     install_requires=["pythonnet"],
     zip_safe=True,
     python_requires=">=3.8",
@@ -20,9 +17,5 @@ setup(
     description="",
     url="https://github.com/AD-SDL/biometra_module/biometra_driver",
     license="MIT License",
-    entry_points={ 
-        'console_scripts': [
-            'biometra_driver = biometra_driver.biometra_driver.biometra:main',
-        ]
-    },
+    entry_points={},
 )
