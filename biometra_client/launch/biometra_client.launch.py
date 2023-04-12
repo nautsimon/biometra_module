5460b9c9051ee7d4fa1b07e27b642cab43107965
Dozgulbas <dozgulbas@anl.gov>
2023-04-12 11:00:56 -0500

    Added device name into launch parameters

--- biometra_client/launch/biometra_client.launch.py
+++ biometra_client/launch/biometra_client.launch.py
@@ -5,16 +5,34 @@ def generate_launch_description():
 
     launch_d = LaunchDescription()
     
+    robot_name = LaunchConfiguration('robot_name')
+    device_name =  LaunchConfiguration('device_name')
+    
+    declare_use_robot_name_cmd = DeclareLaunchArgument(
+        name='robot_name',
+        default_value="biometra96"
+        description='Flag to accept robot_name')
+    
+    declare_use_robot_name_cmd = DeclareLaunchArgument(
+        name='device_name',
+        default_value="device1"
+        description='Flag to accept robot_name')
+    
     biometra_client = Node(
             package = 'biometra_client',
             namespace = 'std_ns',
             executable = 'biometra_client',
             output = "screen",
-            name='biometraNode'
-        )
+            name = robot_name,
+            parameters = [
+                {"device_name":device_name}
+                ],
+            emulate_tty = True,        
+            )
     
     launch_d = LaunchDescription()
-
+    launch_d.add_action(declare_use_robot_name_cmd)
+    launch_d.add_action(declare_use_robot_name_cmd)
     launch_d.add_action(biometra_client)
 
     return launch_d
